2c7518b55a1b6b1590147626235892778f92f34e
kssosoy <qws1566@naver.com>
2024-02-14 11:19:44 +0900

    소셜 로그인 제외 배포 시도

--- src/passport/googleStrategy.js
+++ src/passport/googleStrategy.js
@@ -1,45 +1,45 @@
-import {Strategy as GoogleStrategy} from 'passport-google-oauth20';
-import passport from 'passport';
-import jwt from "jsonwebtoken";
-import {User} from "../models/user.js";
-export const SocialGoogle =()=>{
-    passport.use(new GoogleStrategy({
-        clientID : process.env.GOOGLE_ID,
-        clientSecret:process.env.GOOGLE_SECRET,
-        callbackURL:'/user/google/callback',
-    },async(accessToken, refreshToken, profile, done)=>{
-        try{
-            const exUser =await User.findBySocialId(profile._json.email)
-            if(exUser){
-                const token =jwt.sign(
-                    {
-                        userId:exUser.ID.toString(),
-                    },
-                    process.env.JWT_SECRET,
-                    { expiresIn: "1h" }
-                ); return done(null, token);
-            }else{
-                const newUser = await User.addSocialUser(
-                    "google",
-                    profile._json.email,
-                    profile.displayName,
-                    null
-                );
-                const token= jwt.sign(
-                    {
-                        userId: newUser.ID.toString(),
-                    },
-                    process.env.JWT_SECRET,
-                    { expiresIn: "1h" }
-                );
-                return done(null,token);
-            } 
-        }catch(error){
-                console.error(error);
-                done(error);
-            }
-        }
-    )
+// import {Strategy as GoogleStrategy} from 'passport-google-oauth20';
+// import passport from 'passport';
+// import jwt from "jsonwebtoken";
+// import {User} from "../models/user.js";
+// export const SocialGoogle =()=>{
+//     passport.use(new GoogleStrategy({
+//         clientID : process.env.GOOGLE_ID,
+//         clientSecret:process.env.GOOGLE_SECRET,
+//         callbackURL:'/user/google/callback',
+//     },async(accessToken, refreshToken, profile, done)=>{
+//         try{
+//             const exUser =await User.findBySocialId(profile._json.email)
+//             if(exUser){
+//                 const token =jwt.sign(
+//                     {
+//                         userId:exUser.ID.toString(),
+//                     },
+//                     process.env.JWT_SECRET,
+//                     { expiresIn: "1h" }
+//                 ); return done(null, token);
+//             }else{
+//                 const newUser = await User.addSocialUser(
+//                     "google",
+//                     profile._json.email,
+//                     profile.displayName,
+//                     null
+//                 );
+//                 const token= jwt.sign(
+//                     {
+//                         userId: newUser.ID.toString(),
+//                     },
+//                     process.env.JWT_SECRET,
+//                     { expiresIn: "1h" }
+//                 );
+//                 return done(null,token);
+//             } 
+//         }catch(error){
+//                 console.error(error);
+//                 done(error);
+//             }
+//         }
+//     )
     
-    );
-    }
+//     );
+//     }
--- src/routes/perfumeList.route.js
+++ src/routes/perfumeList.route.js
@@ -1,9 +1,9 @@
-// import express from "express";
-// import asyncHandler from "express-async-handler";
+import express from "express";
+import asyncHandler from "express-async-handler";
 
-// import { perfumeList } from "../controllers/perfumeList.controller.js";
-// import { isAuth } from "../middlewares/jwt.js";
+import { perfumeList } from "../controllers/perfumeList.controller.js";
+import { isAuth } from "../middlewares/jwt.js";
 
-// export const perfumeListRouter = express.Router({ mergeParams: true });
+export const perfumeListRouter = express.Router({ mergeParams: true });
 
-// perfumeListRouter.get("/", isAuth, asyncHandler(perfumeList));
+perfumeListRouter.get("/", isAuth, asyncHandler(perfumeList));
