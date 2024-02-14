83176254fcd829535dc51af6599a69a6c3af26f4
pookey1104 <pookey1104@naver.com>
2024-02-15 01:23:26 +0900

    swagger server

--- config/swagger.config.js
+++ config/swagger.config.js
@@ -7,17 +7,27 @@ const options = {
       version: "1.0.0",
       description: "PROUST API with express, API 설명",
     },
-    host: "localhost:3000",
-    basepath: "../",
-    securityDefinitions: {
-      // 보안 정의 추가
-      BearerAuth: {
-        type: "apiKey",
-        name: "Authorization",
-        scheme: "bearer",
-        in: "header",
+    components: {
+      securityDefinitions: {
+        // 보안 정의 추가
+        BearerAuth: {
+          type: "apiKey",
+          name: "Authorization",
+          scheme: "bearer",
+          in: "header",
+        },
       },
     },
+    servers: [
+      {
+        url: "http://dev.proust.store",
+        description: "API 서버",
+      },
+      {
+        url: "http://localhost:3000",
+        description: "로컬호스트",
+      },
+    ],
   },
   apis: ["./src/routes/*.js", "./swagger/*"],
 };
